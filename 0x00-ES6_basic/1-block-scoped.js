3dca4486b2fd6e80c92e9c530fff41ad9aef5988
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-04-22 19:21:30 +0000

    ES6  Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,9 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  let task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    task = true;
   }
-
   return [task, task2];
 }
