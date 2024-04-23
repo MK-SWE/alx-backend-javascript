74fd8bfa869efe2e77115cb83578696cdd9949df
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-04-23 12:09:04 +0000

    iterateThroughObject

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -2,8 +2,8 @@ export default function iterateThroughObject(reportWithIterator) {
   let report = '';
   reportWithIterator.forEach((el, i) => {
     report += el;
-    if (i < reportWithIterator.length -1) {
-      report += ` | `;
+    if (i < reportWithIterator.length - 1) {
+      report += ' | ';
     }
   });
   return report;
