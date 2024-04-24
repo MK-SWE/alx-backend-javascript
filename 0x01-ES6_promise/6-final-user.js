9a1d48cc4d420273f10ab53398c1e718cce4e7e8
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-04-24 23:52:44 +0000

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,10 +1,10 @@
-import signUpUser from "./4-user-promise";
+import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const user = signUpUser(firstName, lastName);
-  const photo = uploadPhoto(fileName)
-  return Promise.allSettled([user, photo]).then((values) =>{
+  const photo = uploadPhoto(fileName);
+  return Promise.allSettled([user, photo]).then((values) => {
     const res = [];
     values.forEach((val) => {
       if (val.status === 'fulfilled') {
@@ -12,7 +12,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
       } else {
         res.push({ status: val.status, value: `${val.reason}` });
       }
-      return res;
-    })
-  })
+    });
+    return res;
+  });
 }
