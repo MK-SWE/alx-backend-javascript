dd10568ffc58f1ba2875652572e649aa302f7ec8
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-04-26 01:35:16 +0000

    abstract class 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -3,8 +3,8 @@ export default class Building {
     if (new.target === Building) {
       throw new Error('Cannot instantiate abstract class');
     }
-    if (!this.evacuationWarningMessage) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    if (this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
     if (typeof sqft !== 'number') {
       throw new TypeError('sqft must be a number');
