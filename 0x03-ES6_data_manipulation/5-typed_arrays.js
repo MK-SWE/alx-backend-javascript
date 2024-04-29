2b93493370b14d5ec2288785aae21a2d4604a56f
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-04-29 23:21:24 +0000

    Typed Array 5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,7 @@
 export default function createInt8TypedArray(len, pos, val) {
-  const uint8 = new Uint8Array(len);
   if (pos > len) throw new Error('Position outside range');
-  uint8[pos] = val;
-  return uint8;
+  const buffer = new ArrayBuffer(len);
+  const view = new DataView(buffer);
+  view.setInt8(pos, val);
+  return view;
 }
