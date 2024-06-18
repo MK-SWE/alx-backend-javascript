a4ea5523788645702b9828c1026ad20881f25b5a
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-06-18 17:41:53 +0300

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,20 +1,16 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const line = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-line.question('Welcome to Holberton School, what is your name?\n', (msg) => {
-  // Print the message
-  console.log(`Your name is: ${msg}`);
-
-  // Close the readline interface
-  line.close();
-});
-
-// Handle the 'close' event
-line.on('close', () => {
-  console.log('This important software is now closing\n');
-  process.exit(0);
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
