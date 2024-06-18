875f8bac92eda3a12ed57557e4efcaad3c5bf5d6
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-06-18 20:05:22 +0300

    Using Process stdin

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    // Read the file synchronously
+    const fileData = fs.readFileSync(path, { encoding: 'utf8', flag: 'r' });
+
+    // Split the data into lines
+    const filerows = fileData.split('\n').filter((line) => line !== '');
+
+    // Remove the header
+    filerows.shift();
+
+    // Count the students
+    const students = filerows.map((line) => line.split(','));
+    console.log(`Number of students: ${students.length}`);
+
+    // Count the students in each field
+    const fields = {};
+    students.forEach((student) => {
+      if (!fields[student[3]]) {
+        fields[student[3]] = [];
+      }
+      fields[student[3]].push(student[0]);
+    });
+
+    Object.keys(fields).forEach((field) => {
+      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+    });
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
