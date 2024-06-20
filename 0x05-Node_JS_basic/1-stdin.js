7d56608a745eacd8a6fbbb2f14ff0b96e28a4aec
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-06-21 00:06:56 +0300

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,6 +6,10 @@ if (process.stdin.isTTY) {
     process.exit();
   });
 } else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
   process.on('exit', () => {
     process.stdout.write('This important software is now closing\n');
   });
