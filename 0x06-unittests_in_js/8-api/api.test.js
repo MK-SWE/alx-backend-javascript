79e58f09467b84776a064c281c46b9d6e2b8fbf8
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-06-20 22:11:32 +0300

    test server

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,18 +1,27 @@
 const request = require("request");
-const assert = require('assert');
+const expect = require("chai").expect;
 
-describe('Index page', () => {
-  const url = 'http://localhost:7865';
-
-  it('Test status code', () => {
-    request(url, (err, res, body) => {
-      assert.strictEqual(res.statusCode, 200);
+describe("Index page", () => {
+  const options = {
+    url: "http://localhost:7865/",
+    method: "GET"
+  }
+  it("Test status code", (done) => {
+    request(options, (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      done();
     });
   });
-
-  it('Test status code', () => {
-    request(url, (err, res, body) => {
-      assert.strictEqual(body, 'Welcome to the payment system');
+  it("Test response body", (done) => {
+    request(options, (err, res, body) => {
+      expect(body).to.contain("Welcome to the payment system");
+      done();
+    });
+  });
+  it("Test content length", (done) => {
+    request(options, (err, res, body) => {
+      expect(res.headers['content-length']).to.equal('29');
+      done();
     });
   });
 });
--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -6,6 +6,12 @@ app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
 app.listen(7865, '127.0.0.1', () => {
   console.log('API available on localhost port 7865');
 });
+
+module.exports = app;
